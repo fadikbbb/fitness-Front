031170a02841b2756b62415f0c95a138d29aa198
Hadi Kabbani <hadikbb76917223@gmail.com>
2024-08-30 07:40:07 +0300

    marge some data

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {
-      keyframes: {
-        shake: {
-          "0%": { transform: "translateX(0)" },
-          "20%": { transform: "translateX(-10px)" },
-          "40%": { transform: "translateX(10px)" },
-          "60%": { transform: "translateX(-10px)" },
-          "80%": { transform: "translateX(10px)" },
-          "100%": { transform: "translateX(0)" },
-        },
-      },
-      animation: {
-        shake: "shake 0.5s ease-in-out",
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
-
-/* Shake animation */
+}
